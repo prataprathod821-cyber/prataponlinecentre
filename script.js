5dec79909596bdc34fce06bbdce1bfc04fed36ae
prataprathod821-cyber <prataprathod821@gmail.com>
2026-07-14 20:51:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Pratap Online Centre Website Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Pratap Online Centre Website Loaded Successfully!");
+
+    alert("Welcome to Pratap Online Centre!");
+});
